6ebe25d758bea79a055bdf2e5e601df6d741cdb5
lorduskordus <lorduskordus@gmail.com>
2024-11-07 23:23:10 +0100

    Add modernized Linux tools

---
+++ files/system/common/etc/profile.d/modern-unix.sh
@@ -0,0 +1,9 @@
+# Aliases for modernized versions of common Linux tools
+
+# bat for cat
+alias cat='bat --style=plain --pager=never'
+
+# eza for ls
+alias ls='eza'
+alias ll='eza -lHbg --group-directories-first'
+alias la='eza -lHbgaa --group-directories-first'
---
+++ files/system/common/etc/skel/.config/fish/conf.d/modern-unix.fish
@@ -0,0 +1,3 @@
+if test -e /etc/profile.d/modern-unix.sh
+    . /etc/profile.d/modern-unix.sh
+end
