cb36495159e54b91f3959ae49d7e7f55c0294336
lorduskordus <lorduskordus@gmail.com>
2024-12-02 20:36:09 +0100

    Customize Brave's wrapper to include flags from file

---
+++ files/scripts/common/brave-enable-flags.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Customizes Brave's wrapper to include flags from '$HOME/.config/brave-flags.conf'
+
+set -euo pipefail
+
+BRAVE_WRAPPER="/usr/lib/opt/brave.com/brave/brave-browser"
+
+BRAVE_FLAGS_FILE='$HOME/.config/brave-flags.conf'
+OPTIONS='$([ -f "'"$BRAVE_FLAGS_FILE"'" ] \&\& cat "'"$BRAVE_FLAGS_FILE"'" | sed '"'s/#.*//'"')'
+
+ORIGINAL_STR='"$HERE/brave" "$@" || true'
+NEW_STR='"$HERE/brave" "$@" "'$OPTIONS'" || true'
+
+if [ -f "$BRAVE_WRAPPER" ]; then
+    echo "Found the wrapper '$BRAVE_WRAPPER'."
+else
+    echo "Error: Could not find the wrapper '$BRAVE_WRAPPER'."
+    echo "Either Brave is not installed, or someone upstream f'd up and changed how Brave is installed."
+    exit 1
+fi
+
+if grep -qF "$ORIGINAL_STR" "$BRAVE_WRAPPER"; then
+    echo "Found correct string in the wrapper."
+else
+    echo "Error: Could not find string '$ORIGINAL_STR' in the wrapper."
+    echo "Someone upstream probably f'd up and changed the string."
+    exit 1
+fi
+
+if sed -i "s:$ORIGINAL_STR:$NEW_STR:" "$BRAVE_WRAPPER"; then
+    echo "Successfuly modified the wrapper."
+else
+    echo "Failed to modify the wrapper."
+    exit 1
+fi
