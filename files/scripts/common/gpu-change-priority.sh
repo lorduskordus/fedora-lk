6b81a072a0024354b3254925e61d8b98ee96e320
lorduskordus <lorduskordus@gmail.com>
2025-01-23 17:06:57 +0100

    Simplify 'gpu-change-priority' script

--- files/scripts/common/gpu-change-priority.sh
+++ files/scripts/common/gpu-change-priority.sh
@@ -5,18 +5,18 @@
 set -euo pipefail
 
 FILE_DIR="/usr/share/glvnd/egl_vendor.d"
-FILENAME_ORIG="10_nvidia.json"
-FILENAME_NEW="90_nvidia.json"
+FILE_ORIG="${FILE_DIR}/10_nvidia.json"
+FILE_NEW="${FILE_DIR}/90_nvidia.json"
 
 echo "Changing GPU priority"
-if [ -f "${FILE_DIR}/${FILENAME_ORIG}" ]; then
-    if mv "${FILE_DIR}/${FILENAME_ORIG}" "${FILE_DIR}/${FILENAME_NEW}" &> /dev/null; then
+if [ -f "${FILE_ORIG}" ]; then
+    if mv "${FILE_ORIG}" "${FILE_NEW}" &> /dev/null; then
         echo "- Successfully changed priority"
     else
         echo "- Failed to change priority"
         exit 1
     fi
 else
-    echo "- Could not find the file '"${FILE_DIR}/${FILENAME_ORIG}"'"
+    echo "- Could not find the file '"${FILE_ORIG}"'"
     exit 1
 fi
